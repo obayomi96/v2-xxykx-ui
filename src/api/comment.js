6f50e7c44414d7bd08af1220b0c2340724671239
Martins Obayomi <obayomimartins96@gmail.com>
2021-12-15 11:15:43 +0100

    Fetch comments

--- src/api/comment.js
+++ src/api/comment.js
@@ -1,5 +1,33 @@
-//  comments api calls
-const fetchComments = async () => {
-  const res = await fetch(`${process.env.REACT_APP_API_URL}/comments`);
-  return res.json();
+//  Comment api calls
+
+import http from '../utils/http'
+class CommentService {
+
+  fetchComments = async () => {
+    try {
+      const response = await http.get('comments');
+      if (response.status === 200) {
+        const { comment } = response.data;
+        return comment.slice(0, 20)
+      }
+    } catch(error) {
+      return error
+    }
+  }
+
+  AddComment = async (credentials) => {
+    try {
+      const response = await http.post('comments', { comment: { ...credentials }})
+      if (response.status === 201) {
+        const { comment } = response.data;
+        return comment
+      }
+    } catch(error) {
+      return error
+    }
+  }
+
 }
+
+export default CommentService;
+
--- src/pages/Comments/index.jsx
+++ src/pages/Comments/index.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/Header'
+import CommentService from '../../api/comment';
+
+const commentService = new CommentService();
+
+const { fetchComments } = commentService;
 
 function Comments() {
+  const [ commentsList, setCommentsList ] = useState([])
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await fetchComments();
+      setCommentsList(response);
+    }
+    loadData();
+  }, [commentsList]);
+
   return (
     <div className="App">
       <Header />
       comments page
+      <div>
+        <ul>
+          {
+            commentsList.map((comment) => {
+              <li key={comment.id}>{comment.text}</li>
+            })
+          }
+        </ul>
+      </div>
     </div>
   );
 }
